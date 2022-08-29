8db738c6be200a01b7f10ed7cb5780dbae78ff15
Philip Meshach <philipmeshach.pm@gmail.com>
2022-08-29 14:37:24 +0530

    Final commit

--- src/main/java/com/jap/ReverseStringDemo.java
+++ src/main/java/com/jap/ReverseStringDemo.java
@@ -1,21 +1,39 @@
 package com.jap;
 
+import java.util.Scanner;
+
 public class ReverseStringDemo {
     public static void main(String[] args) {
 
-
+        // Creating a new scanner object that reads from the standard input stream.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the the word");
 
         //Create a String object.
+        String string = scanner.nextLine();
+
 
         //Create an object of the class ReverseStringDemo
+        ReverseStringDemo reverseStringDemo = new ReverseStringDemo();
+
 
         //Call the method getReverseString() and pass the parameter
-
+        String reversedString = reverseStringDemo.getReverseString(string);
+        System.out.println("reversedString = " + reversedString);
 
     }
-//Write the logic to reverse the String  inside the below method and return the reverse String.
-public String getReverseString(String string){
 
-        return "";
+    //Write the logic to reverse the String  inside the below method and return the reverse String.
+    public String getReverseString(String string) {
+        String reverseString = " ";
+        // Getting the character at the index and adding it to the reverseString.
+        for (int index = string.length() - 1; index >= 0; index--) {
+            reverseString += string.charAt(index);
+        }
+
+        return reverseString.trim();
     }
+
+
 }
+
